9636701d6f58398cd1498b1928b4147daf61e29e
Ross Mooney <mooney.ross@gmail.com>
2017-11-15 10:54:33 -0500

    Initial Commit; created project

---
+++ app/src/main/java/com/development/rossmooney/explore/LocationsActivity.kt
@@ -0,0 +1,12 @@
+package com.development.rossmooney.explore
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LocationsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_locations)
+    }
+}
